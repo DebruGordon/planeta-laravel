aaca84bc561dd70375201dfb29c1942610c06e3c
Debru <debru.gordon@gmail.com>
2019-08-03 20:43:21 -0300

    Laravel. Rutas armadas. Archivos de "Views" creados. Sgte. paso> armar los "Views" con HTML.

---
+++ Fumigadores/routes/web.php
@@ -0,0 +1,40 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/index', function () {
+    return view('index');
+});
+Route::get('/register', function () {
+    return view('register');
+});
+
+Route::post('/register', function () {
+    return "Bienvenide";
+});
+
+Route::get('/login', function () {
+    return view('login');
+});
+Route::get('/preguntas', function () {
+    return view('preguntas');
+});
+Route::get('/olvidePass', function () {
+    return view('olvidePass');
+});
+Route::get('/passRecuperada', function () {
+    return view('passRecuperada');
+});
